b214c8eca3e2f46c913cbb6b530e7fdec065c587
Диляра <dilyara.mozhet.sadykova@gmail.com>
2022-04-16 10:15:47 +0300

    3

---
+++ RevitAPITrainingLibrary/FurnituresUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class FurnituresUtils
+    {
+        public static List<FamilySymbol> Get
+    }
+}
